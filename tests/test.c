98de09665734a2afbee627a68cded2a1129f7353
Gorruy <vladimirmimikin@gmail.com>
2024-05-23 12:30:25 +0300

    + mmap read check

--- tests/test.c
+++ tests/test.c
@@ -33,7 +33,7 @@ int mmap_check(void)
     FILE *filp4 = fopen(DEV_4, "r+w");
 
     rewind(filp2);
-    res = write(fileno(filp2), buf_to_read, 4096);
+    res = write(fileno(filp2), buf_to_read, PAGE_SIZE);
     if (res < 0) {
         perror("Writing failed\n");
         return -1;
@@ -48,11 +48,11 @@ int mmap_check(void)
     }
     
     memcpy(buf, string, str_size);
-    memset(buf_to_read, 0, PAGE_SIZE);
     msync(buf, PAGE_SIZE, MS_SYNC);
 
     rewind(filp2);
 
+    memset(buf_to_read, 0, PAGE_SIZE);
     res = read(fileno(filp2), buf_to_read, str_size);
     if (res < 0 || res != str_size) {
         fprintf(stderr, "Failed to read from mapped mem,\n");
@@ -62,6 +62,20 @@ int mmap_check(void)
     if (strcmp(string, buf_to_read) != 0) {
         fprintf(stderr, "Wrong data in file after mmap\n");
         return -1;
+    }
+
+    memset(buf_to_read, 'A', 100);
+    rewind(filp2);
+    res = write(fileno(filp2), buf_to_read, 100);
+
+    msync(buf, PAGE_SIZE, MS_SYNC);
+    fsync(fileno(filp2));
+
+    for (int i = 0; i < 100; i++) {
+        if (((char*)buf)[i] != 'A') {
+            fprintf(stderr, "Wrong data in mapped memory after writing to file\n");
+            return -1;
+        }
     }
 
     return 0;

e557c7d011d3f5b0fac0750c60dc60eb3934b078
Gorruy <vladimirmimikin@gmail.com>
2024-05-23 12:24:40 +0300

    new tests

--- tests/test.c
+++ tests/test.c
@@ -8,86 +8,134 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
+#define DEV_1 "/dev/abs_dev-0"
+#define DEV_2 "/dev/abs_dev-1"
+#define DEV_3 "/dev/abs_dev-2"
+#define DEV_4 "/dev/abs_dev-3"
+
+#define PAGE_SIZE 4096
+
+
+int mmap_check(void)
+{
+    void *buf;
+    int res;
+    
+    char buf_to_read[PAGE_SIZE];
+    memset(buf_to_read, 0, PAGE_SIZE);
+
+    char string[] = "Some string to put in mem\n";
+    size_t str_size = strlen(string);
+
+    FILE *filp1 = fopen(DEV_1, "r+w");
+    FILE *filp2 = fopen(DEV_2, "r+w");
+    FILE *filp3 = fopen(DEV_3, "r+w");
+    FILE *filp4 = fopen(DEV_4, "r+w");
+
+    rewind(filp2);
+    res = write(fileno(filp2), buf_to_read, 4096);
+    if (res < 0) {
+        perror("Writing failed\n");
+        return -1;
+    }
+
+    fsync(fileno(filp2));
+
+    buf = mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fileno(filp2), 0);
+    if (buf == MAP_FAILED) {
+        perror("Mmap failed\n");
+        return -1;
+    }
+    
+    memcpy(buf, string, str_size);
+    memset(buf_to_read, 0, PAGE_SIZE);
+    msync(buf, PAGE_SIZE, MS_SYNC);
+
+    rewind(filp2);
+
+    res = read(fileno(filp2), buf_to_read, str_size);
+    if (res < 0 || res != str_size) {
+        fprintf(stderr, "Failed to read from mapped mem,\n");
+        return -1;
+    }
+
+    if (strcmp(string, buf_to_read) != 0) {
+        fprintf(stderr, "Wrong data in file after mmap\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int write_read_check(void) 
+{
+    char buf[PAGE_SIZE];
+    char buf1[PAGE_SIZE];
+
+    memset(buf, 1, PAGE_SIZE);
+    memset(buf1, 0, PAGE_SIZE);
+    int ret;
+
+    FILE *filp1 = fopen(DEV_1, "r+w");
+    FILE *filp2 = fopen(DEV_2, "r+w");
+
+    if (!filp1 || !filp2) {
+        perror("Failed to open file\n");
+        return -1;
+    }
+
+    ret = write(fileno(filp1), buf, PAGE_SIZE);
+    if (ret < 0 || ret != PAGE_SIZE) {
+        perror("Failed to write in first dev\n");
+        return -1;
+    }
+
+
+    // Check if write increments file offset
+    ret = write(fileno(filp1), buf, PAGE_SIZE);
+    if (ret != 0) {
+        perror("Writing to end of file done wrong!\n");
+        return -1;
+    }
+
+    rewind(filp1);
+
+    ret = read(fileno(filp1), buf1, PAGE_SIZE);
+    if (ret < 0 || ret != PAGE_SIZE) {
+        perror("Failed to read from file\n");
+        return -1;
+    }
+
+    for (int i = 0; i < PAGE_SIZE; i++) {
+        if (buf1[i] != 1) {
+            fprintf(stderr, "Data in %d element is wrong", i);
+            return -1;
+        }
+    }
+
+    ret = write(fileno(filp2), buf, PAGE_SIZE * 2);
+    if (ret < 0 || ret != PAGE_SIZE) {
+        perror("Failed to write to second file\n");
+        return -1;
+    }
+
+    fclose(filp1);
+    fclose(filp2);
+
+    return 0;
+
+}
 
 int main(void) 
 {
-    char ch[4096];
-    int ret;
-
-    FILE *file = fopen("/dev/abs_dev-0", "w+r");
-    FILE *file1 = fopen("/dev/abs_dev-1", "w+r");
-    FILE *file2 = fopen("/sys/devices/platform/abs_platform_device.0/abs_address", "wb");
-    FILE *file3 = fopen("/sys/devices/platform/abs_platform_device.0/abs_value", "wb");
-
-    if (!file || !file1 || !file2 || !file3) {
-        perror("Failed to open file\n");
+    if (write_read_check()) {
         exit(1);
     }
 
-    memset(ch, 0, 4096);
-    rewind(file);
+    if (mmap_check()) {
+        exit(1);
+    }
 
-    ch[0] = 0;
-    ret = fwrite(ch, 1, 4, file2);
-    fflush(file2);
-    ch[0] = 'x';
-    ret = fwrite(ch, 1, 1, file3);
-    fflush(file3);
-    ch[0] = 1;
-    ret = fwrite(ch, 1, 4, file2);
-    fflush(file2);
-    ch[0] = 'y';
-    ret = fwrite(ch, 1, 1, file3);
-    fflush(file3);
-    ch[0] = 2;
-    ret = fwrite(ch, 1, 4, file2);
-    fflush(file2);
-    ch[0] = 'z';
-    ret = fwrite(ch, 1, 1, file3);
-    fflush(file3);
-
-    ret = read(fileno(file), ch, 3);
-    ch[3] = '\0';
-
-    printf("There should be <xyz>:\n");
-    printf("%s\n", ch);
-
-    fprintf(file, "Some text\n");
-    fsync(fileno(file));
-
-    rewind(file);
-
-    ret = read(fileno(file), ch, 20);
-
-    printf("Write, read and lseek check. There should be <Some text>:\n");
-    printf("%.*s",20, ch);
-
-    memset(ch, 0, 20);
-
-    ret = read(fileno(file1), ch, 20);
-
-    printf("Files independence check. There should be blank line:\n");
-    printf("%.*s",20, ch);
-    printf("\n");
-
-    memset(ch, 0, 20);
-
-    char *arr;
-    arr = mmap(0, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, fileno(file), 0);
-    arr[0] = 'A';
-    arr[1] = 'B';
-    arr[2] = 'C';
-    arr[3] = 'D';
-    arr[4] = '\n';
-    arr[5] = 0; 
-    fsync(fileno(file));
-
-    rewind(file);
-
-    ret = read(fileno(file), ch, 5);
-
-    printf("Mmap write check. There should be <ABCD>:\n");
-    printf("%.*s",5, ch);
-
-    return 0;
+    printf("All tests passed!\n");
+    exit(0);
 }

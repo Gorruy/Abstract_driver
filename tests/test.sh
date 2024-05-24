d48358e70613f57f283b3a132d89bc1c1c82ba71
Gorruy <vladimirmimikin@gmail.com>
2024-05-24 12:46:42 +0300

    simple script to check sysfs

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,26 @@
 #!/usr/bin/bash
 
+echo 125 > /sys/devices/platform/abs_platform_device.0/abs_value;
+OUTPUT=$( cat /sys/devices/platform/abs_platform_device.0/abs_value );
+if [ "$OUTPUT" != "value:125" ]; then
+echo "Failed to write to abs_value";
+fi
+
+echo 125 > /sys/devices/platform/abs_platform_device.0/abs_address;
+echo 0 > /sys/devices/platform/abs_platform_device.0/abs_value;
+OUTPUT=$( cat /sys/devices/platform/abs_platform_device.0/abs_value );
+if [ "$OUTPUT" != "value:0" ]; then
+echo "Failed to write to abs_value";
+fi
+
+echo 0 > /sys/devices/platform/abs_platform_device.0/abs_address;
+OUTPUT=$( cat /sys/devices/platform/abs_platform_device.0/abs_value );
+if [ "$OUTPUT" != "value:125" ]; then
+echo "Failed to write to abs_value";
+fi
+
+echo 125 > /sys/devices/platform/abs_platform_device.0/abs_address;
+OUTPUT=$( cat /sys/devices/platform/abs_platform_device.0/abs_value );
+if [ "$OUTPUT" != "value:0" ]; then
+echo "Failed to write to abs_value";
+fi
